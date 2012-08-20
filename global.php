97b025eec709a52b15c540de0c2ed021c4f49d64
Tatsh <audvare@gmail.com>
2012-08-19 20:49:12 -0700

    Not sure if these will be used

--- global.php
+++ global.php
@@ -4,7 +4,7 @@ require 'sutra/sLoader.php';
 require 'sutra/functions/all.php';
 require 'kusaba/classes/kLoader.php';
 require 'moor/Moor.php';
-require 'CssMin.php';
-require 'Mollom.php';
+//require 'CssMin.php';
+//require 'Mollom.php';
 
 kLoader::best();
