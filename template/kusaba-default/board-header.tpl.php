d65f2915433b5b654c40e6926e810873e07620f1
Tatsh <audvare@gmail.com>
2012-08-27 07:53:34 -0700

    Add helper to style links

--- template/kusaba-default/board-header.tpl.php
+++ template/kusaba-default/board-header.tpl.php
@@ -1,9 +1,21 @@
-<nav>
+<nav class="board-nav">
+  <div class="wrap-bracket-left">[</div>
   <ul>
+    <?php $count = count($boards); ?>
+    <?php $i = 1; ?>
     <?php foreach ($boards as $board): ?>
       <li>
         <a href="/<?php print $board->getShortURL(); ?>/"><?php print $board->getShortURL(); ?></a>
+        <?php if ($count !== $i): ?>/<?php endif; ?>
+        <?php $i++; ?>
       </li>
     <?php endforeach ?>
+  </ul>
+  <div class="wrap-bracket-right">]</div>
+</nav>
+
+<nav class="after-categories">
+  <ul>
+    <li><span>[</span><a href="/">Home</a><span>]</span></li>
   </ul>
 </nav>
