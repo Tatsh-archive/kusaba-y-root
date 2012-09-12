71453f7079fd9d90ed70e92260943dc7c246be00
Tatsh <audvare@gmail.com>
2012-09-11 20:01:40 -0700

    Include new recaptcha5 library

--- global.php
+++ global.php
@@ -5,6 +5,7 @@ require 'sutra/functions/all.php';
 require 'kusaba/classes/kLoader.php';
 require 'moor/Moor.php';
 require 'htmlpurifier/HTMLPurifier.includes.php';
+require 'recaptcha/recaptcha.php';
 //require 'CssMin.php';
 //require 'Mollom.php';
 
